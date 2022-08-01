7280ea1621e32e885927447528a79b018c4b1300
Tufano Pasquale <pastufwork@gmail.com>
2022-08-01 08:10:40 +0200

    edit forms ok
    Signed-off-by: Tufano Pasquale <pastufwork@gmail.com>

--- modules/base/views/default/EditForm.tmpl.php
+++ modules/base/views/default/EditForm.tmpl.php
@@ -3,15 +3,47 @@
 <br>
 <div class="result">
 
-<form method="POST" action="{{url}}<?=$url; ?>">
-<input type="hidden" name="next_url" value="<?=$next_url?>">
 <?php
+/*
+?>
+
+{{if data::elements}}
+
+    {{foreach data::elements elem}}
+        <form method="POST" action="{{url}}<?=$url; ?>">
+            <input type="hidden" name="next_url" value="{{next_url}}<?=$next_url?>">
+            {{foreach data::structure element_structure}}
+                {{element::value}}
+                {{element_structure::field}}
+                {{element_structure::comment}}
+                {{element_structure::pattern}}
+                {{element_structure::display_name}}
+                {{filename =__DIR__.'/form-elements/'.{{element_structure::html_type}}.'.tmpl.php'}}
+                {{include filename}}
+            {{end foreach element_structure}}
+            <div class="row">
+                <input value="Save" type="submit" class="btn btn-warning col-sm-3 col-lg-2 m-3 top-50 start-0">  
+                <a href="<?=$next_url?>" class="btn btn-secondary col-sm-3 col-lg-2 m-3 top-50 start-0">{{'Go to List'}}</a>  
+                {{if permission::can_delete}}
+                <input value="Delete" type="submit" class="btn btn-danger col-sm-3 col-lg-2 m-3 top-50 end-0" onclick="return confirm('Are you sure?');">
+                {{if permission::can_delete}}
+                <br>
+            </div>
+        </form>
+    {{end foreach elem}}
+
+{{end if data::elements}}
+<?php
+*/
 
 $data = $this->data;
-
-
-foreach($this->data['elements'] as $pos_element=>$element)
+foreach($data['elements'] as $pos_element=>$element)
 {
+?>
+<form method="POST" action="{{url}}<?=$url; ?>">
+<input type="hidden" name="next_url" value="<?=$next_url?>">
+
+<?php
     foreach($this->data['structure'] as $element_structure)
     {
         $filename = realpath(__DIR__.'/form-elements/'.$element_structure['html_type'].'.tmpl.php');
@@ -19,10 +51,21 @@ foreach($this->data['elements'] as $pos_element=>$element)
         $value = $element[$name] ?? ($element_structure['default'] ?? '');
         $comment = $element_structure['comment'] ?? '';
         $pattern = $element_structure['rule'] ?? '';
-        $display_name = $element_structure['display_name'] ?? '';
+        $display_name = $element_structure['display_name'] ?? '';        
+        
         if ($filename)
             include($filename); 
     }
+?>
+
+    <div class="row">
+        <input value="Save" type="submit" class="btn btn-warning col-sm-3 col-lg-2 m-3 top-50 start-0">  
+        <a href="<?=$next_url?>" class="btn btn-secondary col-sm-3 col-lg-2 m-3 top-50 start-0">{{'Go to List'}}</a>  
+        <input value="Delete" type="submit" class="btn btn-danger col-sm-3 col-lg-2 m-3 top-50 end-0" onclick="return confirm('Are you sure?');">
+        <br>
+    </div>
+    </form>
+<?php    
 }
 
 // printr($this);
@@ -32,15 +75,4 @@ foreach($this->data['elements'] as $pos_element=>$element)
 // printr($response);
 
 ?>
-
-<div class="row">
-    <input value="Save" type="submit" class="btn btn-warning col-sm-3 col-lg-2 m-3 top-50 start-0">  
-    <a href="<?=$next_url?>" class="btn btn-secondary col-sm-3 col-lg-2 m-3 top-50 start-0">{{'Go to List'}}</a>  
-    <input value="Delete" type="submit" class="btn btn-danger col-sm-3 col-lg-2 m-3 top-50 end-0" onclick="return confirm('Are you sure?');">
-    <br>
 </div>
-</form>
-</div>
-
-<?php
-// printr($this);
--- modules/base/views/default/ModifyForm.tmpl.php
+++ modules/base/views/default/ModifyForm.tmpl.php
@@ -1,8 +1,6 @@
-{{foreach data::elements elem}}
 <?php 
 $url = '/{{data::metadata::module}}/{{elem::id}}/modify';
 $next_url = '/{{data::metadata::module}}';
 
 include(__DIR__.'/EditForm.tmpl.php');
-?>
-{{end foreach elem}}
+
